999658c626e66d1b1fb532d34560097d96e104c0
Deep Bansode <deepbansodeofficial4205@gmail.com>
2022-04-14 22:43:18 +0530

    added heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const port = process.env.port;
 
 const ejs = require("ejs");
 const res = require("express/lib/response");
